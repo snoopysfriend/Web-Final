3426cea78e6c28b0c014ee6f5f73fe8da02f72f2
snoopysfriend <b05902045@ntu.edu.tw>
2020-12-25 15:49:32 +0800

    Add courseInform

---
+++ backend/routes/courseInform.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import CourseInform from '../models/course'
+
+const router = express.Router()
+
+router.get('/', function(req, res, next){
+    let year = req.query.year
+    let courseId = req.query.courseId
+    console.log(`course infrom courseId ${courseId}`)
+    if (year && courseId) {
+        CourseInform.find()
+                    //.where('year').equals(year)
+                    .where('CourseId').equals(courseId)
+                    .limit(5) 
+                    .exec((err, ress) => {
+                        if (err) {
+                            console.log('Course ${year} ${courseId} not found')
+                            res.status(403).send({message: 'error', content: []})
+                        } else {
+                            res.status(200).send({message: 'success', content: ress})
+                        }
+                    }) 
+    } else {
+        console.log(`missing year or courseId`)
+    }
+
+})
+
+export default router 
