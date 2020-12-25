3d692a14a136d5a1a9210df18b5258ba8653c9b2
snoopysfriend <b05902045@ntu.edu.tw>
2020-12-25 14:55:40 +0800

    Add syllabus

---
+++ backend/routes/syllabus.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import Syllabus from '../models/course'
+
+const router = express.Router()
+
+router.get('/', function(req, res, next){
+    let type = req.query.type
+    let year = req.query.year
+    console.log(`Syllabus year ${year} type ${type}`)
+    Syllabus.find()
+            //.where('year').equals(year)
+           // .where('type').equals(type)
+            .where('DptCode').equals(9010)
+            .limit(5)
+            .exec((err, ress) => {
+                if (err) {
+                    console.log(`Syllabus year ${year} type ${type} not found`)
+                    res.status(403).send({})
+                } else {
+                    console.log(ress)
+                    res.status(200).send({message: ress})
+                }
+            }) 
+})
+
+export default router
