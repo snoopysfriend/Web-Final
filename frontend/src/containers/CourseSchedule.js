a71817cdcc3630deb55a29f00a39fdacd70b9c1b
Tong <b05502072@ntu.edu.tw>
2021-01-20 22:14:58 +0800

    CourseSchedule.js

--- frontend/src/containers/CourseSchedule.js
+++ frontend/src/containers/CourseSchedule.js
@@ -1,32 +1,43 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 //
 // material-ui Library
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core/';
 import { Paper, Typography } from '@material-ui/core/';
+// import { courseBar } from './CourseSchesule/courseBar'
 //
 // Self-Defined
-import theme, { TStyle } from '../styles/myMuiStyles'
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 1300,
-    margin: '24px auto',
-  },
-  cell: {
-    border: '1px solid #000',
-    padding: '5px',
-    width: '80px',
-    height: '60px',
+const theme = createMuiTheme({
+  overrides: {
+    MuiTableContainer: {
+      root: {
+        maxWidth: '1350px',
+        margin: '24px auto',
+      }
+    },
+    MuiTableCell: {
+      root: {
+        
+      },
+      body: {
+        position: 'relative',
+        padding: '0px',
+        boxSizing: 'border-box',
+        border: '1px solid #000',
+        width: '85px !important',
+        height: '60px',
+      }
+    },
   }
-}, { name: 'Table' });
-
+});
 
 function createSection(name, clock) {
     return { name, clock };
 }
 const SECTIONS = [
-    createSection("0", "7:20~8:10"),
+    // createSection("0", "7:20~8:10"),
     createSection("1", "7:20~8:10"),
     createSection("2", "7:20~8:10"),
     createSection("3", "7:20~8:10"),
@@ -45,29 +56,92 @@ const SECTIONS = [
 ]
 const DAYS = ["X","一","二","三","四","五","六"];
 
-const response = {"message":"success","content":{"CourseId":["82737"],"daytime":[{"courseId":"82737","time":[[21,2],[49,2]],"place":"新302  新302  新302"}],"_id":"6006f8d6724b5b15d8941913","StudId":"6LT9Y","TimStp":"2021-01-19T15:20:54.762Z"}}
+const response =  {"message":"success","content":[{"courseId":"14980","time":[[29,1],[58,2]],"place":"博理112總人  博理112總人"}]}
 
-// {"message":"success","content":{"CourseId":["82737","93001"],"daytime":[{"courseId":"82737","time":[[21,2],[49,2]],"place":"新302  新302  新302"},{"courseId":"93001","time":[[49,2]],"place":"共201"}],"_id":"6006f8d6724b5b15d8941913","StudId":"6LT9Y","TimStp":"2021-01-19T15:20:54.762Z"}}
-// {"message":"success","content":[{"courseId":"82737","time":[[21,2],[49,2]],"place":"新302  新302  新302"},{"courseId":"93001","time":[[49,2]],"place":"共201"}]}
 
+// {"message":"success","content":{"CourseId":["14980"],"daytime":[{"courseId":"14980","time":[[29,1],[58,2]],"place":"博理112總人  博理112總人"}],"_id":"6008203aa7439c3b1bb86ebc","StudId":"XUXXU","TimStp":"2021-01-20T12:21:14.908Z"}}
+// {"message":"success","content":[{"courseId":"14980","time":[[29,1],[58,2]],"place":"博理112總人  博理112總人"}]}
+
+const GetComponents = () => {
+  
+}
 
 
 export default function CourseSchedule() {
-  const classes = useStyles();
+  const [sectionState, setSectionState] = React.useState(Array.from({length: 100}, (v, i) => 0));
+  const [sectionChildren, setSectionChildren] = React.useState(Array.from({length: 100}, (v, i) => []));
+  const [test, setTest] = React.useState(1);
+  console.log(sectionChildren)
 
+  const courseBar = async(sectionValues) => {
+    // console.log(document.getElementById(`section${sectionValue}`).childElementCount);
+    // var findSection = sectionState.find((item) => item.Id == sectionId);
+    // if (!findSection) setSectionState([...sectionState, {Id: sectionId, count:1}]);
+    const newState = sectionState;
+    // const element = [];
+    // const elements = (sectionValue, during) => {
+    //   return (
+    //     <>
+    //     {sectionValues.map(item => {
+    //       newState[item[0]] += 1;
+    //       return (
+    //         <div className='courseBar' style={{'top': `${4 * (newState[item[0]])+1}px`, 'left': '4px', width: `${84*2 - 8}px`}}>courseBar</div>
+    //       )
+    //     })}
+    //     </>
+    //   )
+    // }
+    const elements = [];
+    // const root = [ele,document.getElementById(`section7`)]
+    {sectionValues.map((item, index) => {
+        console.log(`index ${index} section${item[0]}: ${newState[item[0]]}`)
+        newState[item[0]] += 1;
+        const backgroundColor = (index == 2? 'red': 'green');
+        console.log(`section${item[0]}: ${25 * (newState[item[0]]-1)} ${backgroundColor}`)
+
+        const ele = <div className='courseBar' style={{'top': `${25 * (newState[item[0]]-1)}px`, width: `${84*item[1] - 8}px`, 'backgroundColor': `${backgroundColor}`}}>courseBar</div>;
+
+        const root = document.getElementById(`section${item[0]}`);
+
+        elements.push([ele,root])
+      })
+    }
+    elements.map (item => {
+      const ele = item[0];
+      const root = item[1];
+      console.log(ele, root)
+      // root.appendChild(ele)
+      // ReactDOM.render(
+      //   ele, root
+      // );
+    })
+    setTest(2);
+    setSectionState(newState);
+  
+  }
+
+  React.useEffect(() => {
+    // for (var i = 0; i<3; i++) {
+      courseBar([[29,3],[58,2], [29,1]]);
+    // }
+  });
   return (
-    <TableContainer component="div" className={classes.root}>
-      <Table className={classes.table}>
+    <ThemeProvider theme={theme}>
+
+    <TableContainer component="div" >
+      <Table >
         <TableBody>
           {DAYS.map((row, dayIndex) => (
             <TableRow key={row}>
-              <TableCell className={classes.cell} align="center">{row}</TableCell>
+              <TableCell  align="center">{row}</TableCell>
               {
                 SECTIONS.map((section, sectionIndex) => (
                   <TableCell key={section.name} 
-                    className={classes.cell}
+
                     align="center"
-                    id={`section${(dayIndex-1)*13 + sectionIndex}`}
+                    id={`section${(dayIndex-1)*13 + sectionIndex + 1}`}
+                    value={(dayIndex-1)*13 + sectionIndex + 1}
+                    test={test}
                   >
                     {dayIndex==0 && 
                       <>
@@ -75,6 +149,7 @@ export default function CourseSchedule() {
                       <Typography variant="body1">{section.clock}</Typography>
                       </>
                     }
+                    {/* <GetComponents query={(dayIndex-1)*13 + sectionIndex} /> */}
                   </TableCell>
                 ))
               }
@@ -82,6 +157,8 @@ export default function CourseSchedule() {
           ))}
         </TableBody>
       </Table>
+      
     </TableContainer>
-  );
+    </ThemeProvider>
+  )
 }
